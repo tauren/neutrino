48a9908ecdb9d543ce2ad027457483cfe5d357a0
Tauren Mills <tauren.mills@ubnt.com>
2017-03-01 14:18:47 -0800

    Try with babel-plugin-transform-custom-element-classes

--- neutrino-custom.js
+++ neutrino-custom.js
@@ -9,7 +9,7 @@ module.exports = neutrino => {
       .loader('babel', ({ options }) => {
         // Solve problem with HTMLElements: https://github.com/babel/babel/issues/4480
         // Must go first according to: https://github.com/github/babel-plugin-transform-custom-element-classes
-        // options.plugins.unshift(require.resolve('babel-plugin-transform-custom-element-classes'))
+        options.plugins.unshift(require.resolve('babel-plugin-transform-custom-element-classes'))
         return { options };
       });
 };
